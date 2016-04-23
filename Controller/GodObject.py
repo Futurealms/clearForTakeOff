881dee3a5d3b0c8e983ce8303a57216c1c495ce2
andriylazorenko <andriy.lazorenko@gmail.com>
2016-04-23 16:51:39 +0300

    Init

---
+++ Controller/GodObject.py
@@ -0,0 +1,11 @@
+
+def main(departure, arrival, time):
+    '''
+    The function accepts user inputs: departure city, arrival city and time of flight and returns whether the flight
+    :param departure:
+    :param arrival:
+    :param time:
+    :return:
+    '''
+    function_suite
+    return [expression]
