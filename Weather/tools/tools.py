e88d5a1988df1cb3023b6915dcc6461ac178d719
Іван Шпотенко <ivan.shpotenko@gmail.com>
2016-04-23 19:51:35 +0300

    RData

--- Weather/tools/tools.py
+++ Weather/tools/tools.py
@@ -4,15 +4,13 @@ def parse_csv_with_DatesAndIDs(csv_filename):
         for line in f:
             source_data = line.split(',')[3].strip('"')
             source_id = line.split(',')[5].strip('"')
-            print(source_id,source_data)
+            print(source_id, source_data)
             # airport_ids.append(source_id)
     # airport_ids = airport_ids[1:]
     # print(airport_ids)
     return (airport_ids)
 
-# def
 
 if __name__ == '__main__':
     # print(parse_csv_with_DatesAndIDs('subset.csv'))
-
     pass
--- Weather/wunderwaffle.py
+++ Weather/wunderwaffle.py
@@ -28,7 +28,65 @@ def parse_airport_IDs(csv_filename):
     return (airport_ids)
 
 
+def clearoff_rb_on_csv(csv_filename):
+    with open(csv_filename) as f:
+        airport_ids = []
+        bundle = []
+        for line in f:
+            source_data = line
+            # source_id = line.split(',')[5].strip('"')
+            # print(source_data)
+            source_1 = source_data[:-7].split(',')
+            print(source_1)
+            airport_ids.append(source_1)
+            i = iter(source_1)
+            bundle.append(dict(zip(i, i)))
+    return bundle
+
+    # airport_ids.append(source_id)
+    # airport_ids = airport_ids[1:]
+    # print(airport_ids)
+    return (airport_ids)
+
+
+def insert_or_update(db_name, db_table, entry_list):
+    '''
+    Бере список фільмів і закидає їх у таблицю бази даних.
+    Оновлює запис (або створює новий) у базі даних.
+    :param db_name:     назва бази даних (створить нову, якщо нема)
+    :param db_table:    назва таблиці у базі даних (створить нову, якщо нема)
+    :param entry_list:  список назв фільмів, дані яких треба закинути у таблицю
+    :return:
+    '''
+    import dataset
+    k = 1
+    db = dataset.connect(db_name)
+    table = db[db_table]
+    for entry in entry_list:
+        dict_by_title = imdb_get_dictionary_by_title(entry)
+        existed_id = table.find_one(imdb_id=dict_by_title['imdb_id'])
+        # print(existed_id)
+        if existed_id != None:
+            # print(existed_id, 'funn')
+            if table.find_one(imdb_id=dict_by_title['imdb_id'])['imdb_id'] == dict_by_title['imdb_id']:
+                table.update(dict_by_title, ['imdb_id'])
+                # print('\nentry updateted')
+        else:
+            # print(existed_id, 'funn')
+            table.insert(dict_by_title)
+            # print('\nnew entry created')
+        # drawProgressBar(k / len(entry_list))
+        k += 1
+        users = db['user'].all()
+        # except:
+        #     print('\n', entry, ': sorry, some error ocurred :(')
+        #     k += 1
+    print('all done!')
+
+
 if __name__ == '__main__':
     # airport_ids=parse_airport_IDs("airport_identifiers_andrew.csv")
     # get_weath_hist(airport_ids)
+    print(clearoff_rb_on_csv('00_LAX.csv'))
+
     pass
