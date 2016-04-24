d78a29a07c9744d29b418ace0768ee5c4f21b688
BogdanShevchenko <bogdan-shevchenko@yandex.ua>
2016-04-24 03:13:30 +0300

    REST api

--- settings.py
+++ settings.py
@@ -1,7 +1,21 @@
+request_schema = {'from': {'type': 'string',
+                           'minlength': 3,
+                           'maxlength': 3},
+                  'to': {'type': 'string',
+                         'minlength': 3,
+                         'maxlength': 3},
+                  'day_time': {'type': 'datetime'},
+                  'more_than_one_hour': {'type': 'float'},
+                  'more_than_two_hours': {'type': 'float'}
+               }
+
+
 DOMAIN = {
     'weather': {},
-    'delays': {}
+    'delays': {},
+    'request': {'resource_methods': ['GET', 'POST'], 'schema': request_schema}
 }
+
 
 RESOURCE_METHODS = ['GET', 'DELETE']
 ITEM_METHODS = ['GET', 'PUT', 'DELETE']
